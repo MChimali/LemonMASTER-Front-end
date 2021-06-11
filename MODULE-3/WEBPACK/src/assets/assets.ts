31eb6dae4ac3ba7cecfe73adfe9a9b26cdba4d52
Alpi <chimalimiguel@gmail.com>
2021-06-11 13:52:28 +0200

    add module3

---
+++ MODULE-3/WEBPACK/src/assets/assets.ts
@@ -0,0 +1,26 @@
+import logo from "./logo.png";
+import pic1 from "./top/1.jpg";
+import pic2 from "./top/2.jpg";
+import pic3 from "./top/3.jpg";
+import pic4 from "./top/4.jpg";
+import pic5 from "./top/5.jpg";
+
+import pic6 from "./line-1/1.jpg";
+import pic7 from "./line-1/2.jpg";
+import pic8 from "./line-1/3.jpg";
+import pic9 from "./line-1/4.jpg";
+import pic10 from "./line-1/5.jpg";
+import pic11 from "./line-1/6.jpg";
+
+import pic12 from "./line-2/1.jpg";
+import pic13 from "./line-2/2.jpg";
+import pic14 from "./line-2/3.jpg";
+import pic15 from "./line-2/4.jpg";
+import pic16 from "./line-2/5.jpg";
+import pic17 from "./line-2/6.jpg";
+
+
+
+export const top = [logo, pic1, pic2, pic3, pic4, pic5];
+export const line1 = [pic6, pic7, pic8, pic9, pic10, pic11];
+export const line2 = [pic12, pic13, pic14, pic15, pic16, pic17];
---
+++ MODULE-3/WEBPACK/src/index.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.scss"
+import {top, line1, line2} from './assets/assets';
+
+console.log(`${process.env.API_BASE}`)
+
+ReactDOM.render(   
+    <> 
+    <header>
+      <div className="header-logo">
+        <img src={top[0]} />
+        <div className="platform-title">Warner Live</div>
+      </div>
+      <div className="header-buttons">
+        <div>Inicio</div>
+        <div>Buscar</div>
+      </div>
+    </header>
+    <main>
+      <div className="section-title">
+        Los <span id="top-text"></span> más populares hoy
+      </div>
+      <section className="top">
+        <div  className="top-item"><img src={top[1]} /></div>
+        <div className="top-item"><img src={top[2]} /></div>
+        <div className="top-item"><img src={top[3]} /></div>
+        <div className="top-item"><img src={top[4]} /></div>
+        <div className="top-item"><img src={top[5]} /></div>
+      </section>
+      <div className="section-title">Comedias</div>
+      <section className="first">
+        <div className="item"><img src={line1[0]} /></div>
+        <div className="item"><img src={line1[1]} /></div>
+        <div className="item"><img src={line1[2]} /></div>
+        <div className="item"><img src={line1[3]} /></div>
+        <div className="item"><img src={line1[4]} /></div>
+        <div className="item"><img src={line1[5]} /></div>
+      </section>
+      <div className="section-title">Dramas basados en libros</div>
+      <section className="second">
+        <div className="item"><img src={line2[0]} /></div>
+        <div className="item"><img src={line2[1]} /></div>
+        <div className="item"><img src={line2[2]} /></div>
+        <div className="item"><img src={line2[3]} /></div>
+        <div className="item"><img src={line2[4]} /></div>
+        <div className="item"><img src={line2[5]} /></div>
+      </section>
+    </main></>,
+    document.getElementById("root")
+  );
